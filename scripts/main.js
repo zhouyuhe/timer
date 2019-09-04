5dd17b4ff97934501ebf77ffe8912ea55ebac314
Yuhe Zhou <yuhe.zhou@weareadaptive.com>
2019-09-04 18:34:33 +0100

    efficient

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,55 @@
 //comment
 
-let milisec = 0;
-let timerStatus = 'start';
+class Timer {
+    constructor(onUpdate) {
+        this.timeElapsed = 0
+        this.isRunning = false
+        this.interval = null
+        this.onUpdate = onUpdate
+        this.startTimer = () => {
+            this.isRunning = true
+            this.interval = setInterval(() => {
+                this.timeElapsed++
+                this.onUpdate()
+            }, 10)
+        }
 
-function timer() {
-    milisec++;
-    document.getElementById('timer').innerHTML = milisec;
-}
-
-window.setInterval(timer, 1)
-
-function startStop(){
-
-    if (timerStatus==='start'){
-
-        document.getElementById("start-stop").innerHTML="stop";
-        document.getElementById('lap-reset').innerHTML='lap';
-        timerStatus='stop';
+        this.stopTimer = () => {
+            clearInterval(this.interval)
+            this.isRunning = false
+            this.onUpdate()
+        }
     }
-    else{
-        document.getElementById('start-stop').innerHTML='start';
-        document.getElementById('lap-reset').innerHTML='reset';
-        timerStatus='start';
+
+    startStop() {
+        if (this.isRunning === false) {
+            this.startTimer()
+        } else {
+            this.stopTimer()
+        }
     }
-    
 }
 
-var intervalID = window.setInterval(myCallback, 500, 'Parameter 1', 'Parameter 2');
+let timer;
 
-function myCallback(a, b)
-{
- // Your code here
- // Parameters are purely optional.
- console.log(a);
- console.log(b);
+const formatTime = (timeInMiliSeconds) => {
+    return timeInMiliSeconds / 100
 }
+
+const updateTimeLabel = (time) => document.getElementById('timer').innerHTML = time;
+ 
+const resolveButtonText = (isRunning) => isRunning ? 'Stop' : 'Start'
+
+const updateStopStartButton = (text) => document.getElementById('start-stop').innerHTML = text
+
+const resolveResetText = (timeElapsed, isRunning) => isRunning && timeElapsed > 0 ? 'Lap' : 'Reset'
+
+const updateResetButton = (text) => document.getElementById('lap-reset').innerHTML = text
+
+function onTimerUpdate() {
+    updateTimeLabel(formatTime(timer.timeElapsed))
+    updateStopStartButton(resolveButtonText(timer.isRunning))
+    updateResetButton(resolveResetText(timer.timeElapsed, timer.isRunning))
+}
+
+timer = new Timer(onTimerUpdate)
